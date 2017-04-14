c228cfff6fa13e2cd9092f3276a9c55477bb06e2
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-04-14 22:04:15 +0200

    Add save to file #3

--- src/clj_simple_chart/core.clj
+++ src/clj_simple_chart/core.clj
@@ -6,7 +6,11 @@
            (java.util.concurrent CountDownLatch)
            (javafx.scene.web WebView)
            (javafx.scene.layout VBox)
-           (javafx.scene Scene)))
+           (javafx.scene Scene SnapshotParameters)
+           (javafx.geometry Rectangle2D)
+           (javafx.embed.swing SwingFXUtils)
+           (javax.imageio ImageIO)
+           (java.io File)))
 
 (defonce latch (CountDownLatch. 1))
 (defonce webview (atom nil))
@@ -60,3 +64,42 @@
         s (hiccup.core/html (conj body t))]
     (render-string s)
     s))
+
+(defn render-to-file [filename t]
+  (let [ll (CountDownLatch. 1)
+        attrs (second t)
+        width (:width attrs)
+        height (:height attrs)]
+    (render-tag t)
+    (Platform/runLater (fn [] (.sizeToScene @stage)))
+    (render-tag t) ; TODO: Can this be simplified?
+    (Platform/runLater
+      (fn []
+        (let [snap (SnapshotParameters.)]
+          (.setViewport snap (Rectangle2D. 0 0 width height))
+          (let [image (.snapshot @webview snap nil)
+                bufimage (SwingFXUtils/fromFXImage image nil)]
+            (ImageIO/write bufimage "png" (File. filename))
+            (println "\nwrote " filename)))))
+    (Platform/runLater (fn [] (.countDown ll)))
+    (.await ll)))
+
+
+(def width 960)
+(def height 500)
+(def margin {:top 10 :bottom 10 :left 10 :right 10})
+
+(render-to-file
+  "hello.png"
+  [:svg {:width  (+ (:left margin) (:right margin) width)
+         :height (+ (:top margin) (:bottom margin) height)}
+   [:circle {:cx 0 :cy 0 :r 50 :fill "yellow" :stroke "black"}]
+   [:g {:transform (translate (:left margin) (:top margin))}
+    [:rect {:x 0 :y 0 :width width :height height :fill "none" :stroke "red"}]
+    [:circle {:cx 50 :cy 100 :r 10 :fill "yellow" :stroke "black"}]
+    [:circle {:cx 50 :cy 100 :r 1 :fill "yellow" :stroke "black"}]
+    [:line {:x1 50 :y1 100 :x2 width :y2 100 :stroke "red"}]
+    [:text {:x 50 :y 100 :dy ".32em" :font-size "200px"} "1,23456789"]
+    [:line {:x1 0 :y1 0 :x2 width :y2 height :stroke "blue"}]
+    [:line {:x1 width :y1 0 :x2 0 :y2 height :stroke "blue"}]
+    ]])
--- src/clj_simple_chart/exp.clj
+++ src/clj_simple_chart/exp.clj
@@ -1,17 +1,3 @@
 (ns clj-simple-chart.exp
   (:require [clj-simple-chart.core :as sc]))
 
-(def width 1200)
-(def height 500)
-
-(def margin {:top 10 :bottom 10 :left 10 :right 10})
-
-(sc/render-tag [:svg {:width  (+ (:left margin) (:right margin) width)
-                      :height (+ (:top margin) (:bottom margin) height)}
-                [:g {:transform (sc/translate (:left margin) (:top margin))}
-                 [:rect {:x 0 :y 0 :width width :height height :fill "none" :stroke "red"}]
-                 [:circle {:cx 50 :cy 100 :r 10 :fill "yellow" :stroke "black"}]
-                 [:circle {:cx 50 :cy 100 :r 1 :fill "yellow" :stroke "black"}]
-                 [:line {:x1 50 :y1 100 :x2 width :y2 100 :stroke "red"}]
-                 [:text {:x 50 :y 100 :dy ".32em" :font-size "200px"} "1,234567890"]
-                 ]])
