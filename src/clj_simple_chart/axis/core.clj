d5f7c52b681f4db42b1a5a9ca54a37fbe0c05fb3
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-04-22 19:10:44 +0200

    Ticks are quite OK ...

--- src/clj_simple_chart/axis/core.clj
+++ src/clj_simple_chart/axis/core.clj
@@ -1,4 +1,5 @@
-(ns clj-simple-chart.axis.core)
+(ns clj-simple-chart.axis.core
+  (:require [clj-simple-chart.ticks :as tiks]))
 
 (defn translate [x y]
   (str "translate(" x "," y ")"))
@@ -13,6 +14,12 @@
   [scale]
   (:domain scale))
 
+(defmethod ticks :linear
+  [scale]
+  (tiks/ticks (first (:domain scale))
+              (last (:domain scale))
+              (get scale :ticks 10)))
+
 (defmulti center-pos (fn [scale v] (:type scale)))
 
 (defmethod center-pos :ordinal
@@ -20,9 +27,14 @@
   (double (+ (/ (:bandwidth scale) 2)
              ((:point-fn scale) v))))
 
+(defmethod center-pos :linear
+  [scale v]
+  (double ((:point-fn scale) v)))
+
 (defn render-x-axis [scale sign dy]
   (let [color (get scale :color "#000")
         rng (:range scale)
+        neg-sign (* -1 sign)
         sign-char (if (= -1 sign) "-" "")]
     [:g
      [:path {:stroke       color
@@ -31,6 +43,11 @@
              :d            (str "M0.5," sign-char "6 V0.5 H" (int (apply max rng)) ".5 V" sign-char "6")}]
      (map (fn [d] [:g {:transform (translate (center-pos scale d) 0)}
                    [:line {:stroke color :x1 0.5 :x2 0.5 :y2 (* sign 6)}]
+                   (when (:grid scale)
+                     [:line {:stroke         color
+                             :stroke-opacity 0.2
+                             :y2             (* neg-sign (:height scale))
+                             :x1 0.5 :x2 0.5}])
                    [:text (merge text-axis-properties
                                  {:x           0.5
                                   :text-anchor "middle"
@@ -39,7 +56,47 @@
                                   :y           (* sign 9)})
                     (str d)]]) (ticks scale))]))
 
+(defn render-y-axis [scale sign text-anchor]
+  (let [color (get scale :color "#000")
+        sign-char (if (= -1 sign) "-" "")
+        neg-sign (* -1 sign)
+        rng (:range scale)]
+    [:g
+     [:path {:stroke       color
+             :stroke-width "1"
+             :fill         "none"
+             :d            (str "M" sign-char "6," (int (apply max rng)) ".5 H0.5 V" (int (apply min rng)) ".5 H" sign-char "6")}]
+     (map (fn [d] [:g {:transform (translate 0 (center-pos scale d))}
+                   [:line {:stroke color :x2 (* sign 6) :y1 0.5 :y2 0.5}]
+                   (when (:grid scale)
+                     [:line {:stroke         color
+                             :stroke-opacity 0.2
+                             :x2             (* neg-sign (:width scale))
+                             :y1 0.5 :y2 0.5}])
+                   [:text (merge text-axis-properties
+                                 {:x           (* sign 9)
+                                  :text-anchor text-anchor
+                                  :fill        color
+                                  :dy          ".32em"
+                                  :y           0.5})
+                    (str d)]]) (ticks scale))]))
+
 (defmulti render-axis (juxt :axis :orientation))
+
+(defmethod render-axis [:y :left] [scale]
+  [:g {:transform (translate 0 0)}
+   (render-y-axis scale -1 "end")])
+
+(defmethod render-axis [:y :right] [scale]
+  [:g {:transform (translate (:width scale) 0)}
+   (render-y-axis scale 1 "start")])
+
+(defmethod render-axis [:y :both] [scale]
+  [:g
+   (render-axis (-> scale
+                    (assoc :orientation :left)
+                    (dissoc :grid)))
+   (render-axis (assoc scale :orientation :right))])
 
 (defmethod render-axis [:x :bottom] [scale]
   [:g {:transform (translate 0 (:height scale))}
@@ -51,5 +108,7 @@
 
 (defmethod render-axis [:x :both] [scale]
   [:g
-   (render-axis (assoc scale :orientation :top))
+   (render-axis (-> scale
+                    (assoc :orientation :top)
+                    (dissoc :grid)))
    (render-axis (assoc scale :orientation :bottom))])
--- src/clj_simple_chart/demo.clj
+++ src/clj_simple_chart/demo.clj
@@ -1,24 +1,27 @@
 (ns clj-simple-chart.demo
   (:require [clj-simple-chart.core :refer :all]))
 
-(def margin {:top 60 :bottom 40 :left 40 :right 40})
+(def margin {:top 75 :bottom 40 :left 40 :right 40})
 (def width (- (/ 1024 2) (:left margin) (:right margin)))
 (def height (- (/ 512 2) (:top margin) (:bottom margin)))
 
 (def x (scale {:type          :ordinal
                :axis          :x
-               :orientation   :bottom
+               :orientation   :both
                :width         width
                :height        height
+               :grid          true
                :domain        (range 1990 (inc 1995) 1)
                :padding-inner 0.1
                :padding-outer 0.1}))
 
-(def y (scale {:type   :linear
-               :axis   :y
-               :width  width
-               :height height
-               :domain [0 100]}))
+(def y (scale {:type        :linear
+               :axis        :y
+               :grid        true
+               :orientation :both
+               :width       width
+               :height      height
+               :domain      [0 100]}))
 
 #_(def rect (partial rect/vertical-rect x y))
 
@@ -27,6 +30,7 @@
    (title "Some values")
    (sub-title "1990 - 1995")
    [:g {:transform (translate (:left margin) (:top margin))}
+    (render-axis y)
     (render-axis x)
     ;[:g (left-y-axis y)]
     ;[:g {:transform (translate width 0)} (right-y-axis y)]
