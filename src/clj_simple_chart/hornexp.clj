e08f2be63ea048449e8398b7452fac7b1ad18058
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-04-25 22:40:01 +0200

    Reproduce nashorn error... take two...

--- src/clj_simple_chart/hornexp.clj
+++ src/clj_simple_chart/hornexp.clj
@@ -14,4 +14,14 @@
 (def font-bytes (FileUtils/readFileToByteArray (File. "./resources/fonts/Roboto-Black.ttf")))
 (.eval engine (str "var base64 = '" (String. (base64/encode-bytes font-bytes)) "';"))
 (.eval engine "var ab = b64.decode(base64);")
-(.eval engine "var font = opentype.parse(ab);") ;;; doesn't work
+;(.eval engine "var font = opentype.parse(ab);") ;;; doesn't work
+
+(.eval engine "
+function uint32(x) {
+  var buffer = new ArrayBuffer(8);
+  var dataview = new DataView(buffer);
+  dataview.setUint32(0, x);
+  return dataview.getUint32(0, false);
+}
+print(uint32(0x5F0F3CF5) === uint32(0x5F0F3CF5));
+")
