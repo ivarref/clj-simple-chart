a26bec3d22208fd08376a8f15d72f9fdceada7ab
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-04-28 21:24:56 +0200

    Add simple opentype.js demo

--- src/clj_simple_chart/demoopentype.clj
+++ src/clj_simple_chart/demoopentype.clj
@@ -2,23 +2,24 @@
   (:require [clj-simple-chart.core :refer :all]
             [clj-simple-chart.opentype :as opentype]))
 
-(def margin {:top 75 :bottom 40 :left 40 :right 40})
-(def width (- (/ 1024 2) (:left margin) (:right margin)))
-(def height (- (/ 512 2) (:top margin) (:bottom margin)))
+(defn bounding-box-lines [bb margin stroke]
+  [:g
+   [:line {:x1 (- (:x1 bb) margin) :x2 (+ (:x2 bb) margin) :y1 (- (:y1 bb) margin) :y2 (- (:y1 bb) margin) :stroke stroke}]
+   [:line {:x1 (- (:x1 bb) margin) :x2 (+ (:x2 bb) margin) :y1 (+ (:y2 bb) margin) :y2 (+ (:y2 bb) margin) :stroke stroke}]
+   [:line {:x1 (- (:x1 bb) margin) :x2 (- (:x1 bb) margin) :y1 (- (:y1 bb) margin) :y2 (+ (:y2 bb) margin) :stroke stroke}]
+   [:line {:x1 (+ (:x2 bb) margin) :x2 (+ (:x2 bb) margin) :y1 (- (:y1 bb) margin) :y2 (+ (:y2 bb) margin) :stroke stroke}]])
+
+(def txt-with-args ["Roboto Regular" "Wow — It Just Works™!" 10 70 30])
 
 (defn diagram []
-  [:svg (svg-attrs width height margin)
+  [:svg {:width 510 :height 256}
    [:path {:d (opentype/get-path-data "Roboto Black"
                                       "Opentype.js integration on the JVM is working!"
                                       10
                                       30
                                       23)}]
-   [:path {:d (opentype/get-path-data "Roboto Regular"
-                                      "Wow — It just works!"
-                                      10
-                                      70
-                                      30)}]
-   ])
+   [:path {:d (apply opentype/get-path-data txt-with-args)}]
+   (bounding-box-lines (apply opentype/get-bounding-box txt-with-args) 5 "red")])
 
 (defn render-self []
   (render "hello.svg" (diagram)))
