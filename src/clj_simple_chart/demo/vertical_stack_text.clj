86e69222f5c93603cce6e32d97f37c2fa76222d0
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-05-05 11:10:51 +0200

    Clean up formatting

--- src/clj_simple_chart/demo/vertical_stack_text.clj
+++ src/clj_simple_chart/demo/vertical_stack_text.clj
@@ -1,7 +1,6 @@
 (ns clj-simple-chart.demo.vertical-stack-text
   (:require [clj-simple-chart.core :refer :all]
-            [clj-simple-chart.opentype :as opentype]
-            [clj-simple-chart.axis.core :as axis]))
+            [clj-simple-chart.opentype :as opentype]))
 
 (defn header-txt [y-offset {path :path idx :idx}]
   [:g {:transform (translate (- (:x1 (meta path))) (reduce + (take idx y-offset)))} path])
@@ -21,16 +20,14 @@
 
 (def header (header-txts [{:text "Hello World" :border-tight "none" :font "Roboto Bold" :font-size 36}
                           {:text "It's gonna stack" :border-tight "none" :font "Roboto Black" :font-size 16}
-                          {:text "Just like java.util.Stack" :border-tight "none" :font-size 16}]))
+                          {:text "Just like java.util.Stack?!" :border-tight "none" :font-size 16}]))
 
 (defn diagram []
   [:svg {:xmlns "http://www.w3.org/2000/svg" :width 250 :height 500}
    [:g {:transform (translate 15.5 15.5)}
     header
     [:g {:transform (translate 0 (:height (meta header)))}
-     [:line {:stroke "black" :x1 -15.5 :x2 250}]
-     ]
-    ]])
+     [:line {:stroke "black" :x1 -15.5 :x2 250}]]]])
 
 (defn render-self []
   (render "./img/vertical-stack-text.png" "./img/vertical-stack-text.svg" (diagram)))
