f1110a1f67ebf47e7adbcff864ca20e52b52eb7a
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-05-15 20:21:47 +0200

    Some work on diagram

--- src/clj_simple_chart/ssb/petroskattdiagrammonthly.clj
+++ src/clj_simple_chart/ssb/petroskattdiagrammonthly.clj
@@ -3,7 +3,8 @@
             [clj-simple-chart.core :as core]
             [clj-simple-chart.chart :as chart]
             [clj-simple-chart.axis.core :as axis]
-            [clj-simple-chart.rect :as rect]))
+            [clj-simple-chart.rect :as rect]
+            [clj-simple-chart.opentype :as opentype]))
 
 (def data (->> petroskatt/twelve-mma-mrd
                (mapv #(assoc % :year (read-string (subs (:dato %) 0 4))))
@@ -14,11 +15,41 @@
 (def svg-width 960)
 (def svg-height 480)
 
-
-(def x-ticks (filter #(.endsWith % "-01") (map :dato data)))
+(def marg 15)
 
 (def ordinaer (keyword "Ordinær skatt på utvinning av petroleum"))
 (def saerskatt (keyword "Særskatt på utvinning av petroleum"))
+
+(def blue "rgb(31, 119, 180)")
+(def orange "rgb(255, 127, 14)")
+(def green "rgb(44, 160, 44)")
+
+(def saerskatt-fill green)
+(def ordinaer-fill blue)
+
+(def header (opentype/text-stack-downwards
+              {:margin-top  marg
+               :margin-left marg
+               :margin-bottom 3}
+              [{:text "Skatteinngang frå kontinentalsokkelen" :font "Roboto Bold" :font-size 36}
+               {:text "Milliardar kroner (løpande), 12 månadar rullande sum" :font "Roboto Black" :font-size 16}
+               {:text "Særskatt på utvinning av petroleum" :fill saerskatt-fill :font "Roboto Black" :font-size 16}
+               {:text "Ordinær skatt på utvinning av petroleum" :fill ordinaer-fill :font "Roboto Black" :font-size 16}]))
+
+(def footer (opentype/text-stack-downwards
+              {:margin-left marg
+               :margin-top 8
+               :margin-bottom 3}
+              [{:text "Kjelde: SSB" :font-size 16}]))
+
+(def footer2 (opentype/text
+              {:text "Diagram: Refsdal.Ivar@gmail.com"
+               :alignment-baseline "hanging"
+               :dy ".5em"
+               :dx (- svg-width marg)
+               :text-anchor "end" :font-size 16}))
+
+(def x-ticks (filter #(.endsWith % "-01") (map :dato data)))
 
 (def xx {:type          :ordinal
          :orientation   :bottom
@@ -35,24 +66,30 @@
          :ticks       5
          :domain      [0 275]})
 
-(def c (chart/chart {:width  svg-width
-                     :height svg-height
+(def available-height (- svg-height (:height (meta header)) (:height (meta footer))))
+
+(def c (chart/chart {:width  (- svg-width marg)
+                     :height available-height
                      :x      xx
                      :y      yy}))
 
 (def bars (rect/scaled-rect (:x c) (:y c)))
 
 (defn make-rect [opts]
-  [{:p (:dato opts) :c ordinaer :h (get opts ordinaer) :fill "steelblue"}
-   {:p (:dato opts) :c saerskatt :h (get opts saerskatt) :fill "red"}])
+  [{:p (:dato opts) :c ordinaer :h (get opts ordinaer) :fill ordinaer-fill}
+   {:p (:dato opts) :c saerskatt :h (get opts saerskatt) :fill saerskatt-fill}])
 
 (defn diagram []
   [:svg {:xmlns "http://www.w3.org/2000/svg" :width svg-width :height svg-height}
-   [:g {:transform (core/translate (:margin-left c) (:margin-top c))}
+   header
+   [:g {:transform (core/translate (+ marg (:margin-left c)) (+ (:height (meta header)) (:margin-top c)))}
     [:g (bars (mapv make-rect data))]
     (axis/render-axis (:y c))
-    (axis/render-axis (:x c))]])
+    (axis/render-axis (:x c))]
+   [:g {:transform (core/translate 0 (+ (:height (meta header)) available-height))} footer]
+   [:g {:transform (core/translate 0 (+ (:height (meta header)) available-height))} footer2]
+   ])
 
 
 (defn render-self []
-  (core/render "./img/petroskatt-monthly-mms.svg" (diagram)))
+  (core/render "./img/petroskatt-mms.svg" "./img/petroskatt-mms.png" (diagram)))
