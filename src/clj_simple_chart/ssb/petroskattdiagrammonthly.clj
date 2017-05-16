357196b747c5dacfa3241408904a8790558a77b5
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-05-16 18:00:34 +0200

    Clean up code a little

--- src/clj_simple_chart/ssb/petroskattdiagrammonthly.clj
+++ src/clj_simple_chart/ssb/petroskattdiagrammonthly.clj
@@ -9,8 +9,12 @@
             [clj-simple-chart.opentype :as opentype]
             [clojure.string :as string]))
 
+(def ordinaer (keyword "Ordinær skatt på utvinning av petroleum"))
+(def saerskatt (keyword "Særskatt på utvinning av petroleum"))
+
 (def data (->> petroskatt/twelve-mma-mrd
                (mapv #(assoc % :year (read-string (subs (:dato %) 0 4))))
+               (mapv #(assoc % :sum (+ (get % ordinaer) (get % saerskatt))))
                #_(filter #(>= (:year %) 2010))))
 
 (def oil-date-to-usd brentoilprice/brent-12-mma-dato-to-usd)
@@ -21,9 +25,6 @@
 (def svg-height 500)
 
 (def marg 10)
-
-(def ordinaer (keyword "Ordinær skatt på utvinning av petroleum"))
-(def saerskatt (keyword "Særskatt på utvinning av petroleum"))
 
 (def blue "rgb(31, 119, 180)")
 (def orange "rgb(255, 127, 14)")
@@ -46,7 +47,7 @@
 
 (def siste-verdi-str
   (str "Per " (date-readable (:dato last-data)) ": "
-       (string/replace (format "%.1f" (+ (get last-data ordinaer) (get last-data saerskatt))) "." ",")
+       (string/replace (format "%.1f" (:sum last-data)) "." ",")
        " mrd kr."))
 
 (def header (opentype/text-stack-downwards
@@ -116,25 +117,28 @@
 (def y (:y c))
 (def y2 (:y c))
 
+(def xfn (partial point/center-point x))
+(def yfn (partial point/center-point y))
+(def y2fn (partial point/center-point y2))
+
 (def bars (rect/scaled-rect (:x c) (:y c)))
 
 (defn make-rect [opts]
   [{:p (:dato opts) :c ordinaer :h (get opts ordinaer) :fill ordinaer-fill}
    {:p (:dato opts) :c saerskatt :h (get opts saerskatt) :fill saerskatt-fill}])
 
-(defn make-txt [opts]
-  (let [dato (:dato opts)
-        year (:year opts)
-        xfn (partial point/center-point x)
-        yfn (partial point/center-point y)
-        summ (+ (get opts ordinaer) (get opts saerskatt))]
-    [:g {:transform (core/translate (xfn dato) (yfn summ))}
-     [:circle {:r 2}]
-     [:line {:stroke "black" :stroke-width 1 :fill "black" :y2 -8}]
-     (opentype/text {:dy "-.71em" :text-anchor "middle" :text (str "(" year ")")})
-     (opentype/text {:dy   "-1.71em" :font "Roboto Bold" :text-anchor "middle"
-                     :text (string/replace (format "%.1f" summ) "." ",")})
-     ]))
+(defn make-txt [{dato :dato year :year summ :sum}]
+  [:g {:transform (core/translate (xfn dato) (yfn summ))}
+   [:circle {:r 2}]
+   [:line {:stroke "black" :stroke-width 1 :fill "black" :y2 -8}]
+   (opentype/text {:dy "-.71em" :text-anchor "middle" :text (str "(" year ")")})
+   (opentype/text {:dy          "-1.71em"
+                   :font        "Roboto Bold"
+                   :text-anchor "middle"
+                   :text        (string/replace (format "%.1f" summ) "." ",")})])
+
+(defn add-oil-price-line []
+  )
 
 (defn diagram []
   [:svg {:xmlns "http://www.w3.org/2000/svg" :width svg-width :height svg-height}
@@ -145,6 +149,7 @@
     (axis/render-axis (:y2 c))
     [:g (bars (mapv make-rect data))]
     [:g (map make-txt end-of-year-data)]
+    [:g (add-oil-price-line)]
     detail
 
     (axis/render-axis (:x c))]
