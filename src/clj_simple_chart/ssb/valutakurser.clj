be3b9121a9576ef97b5c0c9edf15f31824a4fc1b
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-05-17 16:29:11 +0200

    Start add valutakurser

---
+++ src/clj_simple_chart/ssb/valutakurser.clj
@@ -0,0 +1,40 @@
+(ns clj-simple-chart.ssb.valutakurser
+  (:require [clj-http.client :as client]
+            [clj-simple-chart.csv.csvmap :as csv]
+            [clojure.test :as test]))
+
+(defonce resp (client/get "https://raw.githubusercontent.com/ivarref/valutakurser/master/valuta_mnd.csv"))
+(test/is (= 200 (:status resp)))
+
+(def columns (:columns (csv/csv-map (:body resp))))
+(def data (:data (csv/csv-map (:body resp))))
+(test/is (= columns (mapv keyword ["Date" "1 AUD" "100 BGN" "1 BRL" "1 CAD" "100 CHF" "100 CNY" "100 CZK"
+                                   "100 DKK" "1 EUR" "1 GBP" "1 HKD" "100 HRK" "100 HUF" " I44" "100 IDR"
+                                   "1 ILS" "100 INR" "100 JPY" "100 KRW" "1 LTL" "100 MXN" "1 MYR" "1 NZD"
+                                   "100 PHP" "100 PKR" "1 PLN" "100 RON" "100 RUB" "100 SEK" "1 SGD"
+                                   "100 THB" "100 TRY" "100 TWD" " TWI" "1 USD" "1 XDR" "1 ZAR" "100 BYR"
+                                   "1 BYN"])))
+
+(test/is (= "Jan-60" (:Date (first data))))
+
+(def month-to-num (zipmap ["Jan" "Feb" "Mar" "Apr" "May" "Jun" "Jul" "Aug" "Sep" "Oct" "Nov" "Dec"]
+                          [1 2 3 4 5 6 7 8 9 10 11 12]))
+
+(defn get-date [date]
+  (let [month (subs date 0 3)
+        month-numeric (format "%02d" (get month-to-num month))
+        yr (subs date 4 6)
+        yr (if (.startsWith yr "0") (subs yr 1 2) yr)
+        yr-two-digit (read-string yr)
+        yr (if (< yr-two-digit 60) (str "20" (format "%02d" yr-two-digit))
+                                   (str "19" (format "%02d" yr-two-digit)))]
+    (str yr "-" month-numeric)))
+
+(test/is (= "1960-01" (get-date "Jan-60")))
+(test/is (= "2000-01" (get-date "Jan-00")))
+(test/is (= "2008-01" (get-date "Jan-08")))
+(test/is (= "2017-04" (get-date "Apr-17")))
+
+(def parsed (mapv (fn [x] (assoc x :Date (get-date (:Date x)))) data))
+
+(csv/write-csv "./data/valuta_monthly.csv" {:columns columns :data parsed})
