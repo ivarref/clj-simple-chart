cc77a6f8c3c4d6bab580d953326b829fcd74fc5a
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-06-21 21:08:48 +0200

    Start add gdp and population

--- src/clj_simple_chart/bp/wbdata.clj
+++ src/clj_simple_chart/bp/wbdata.clj
@@ -4,12 +4,37 @@
             [clojure.pprint :refer [pprint]]
             [clj-simple-chart.csv.csvmap :as csv]
             [clojure.data.json :as json]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [clojure.set :as set]))
 
 (def cached-get (memoize client/get))
 
 (def cc2 (json/read-str (:body (cached-get "https://raw.githubusercontent.com/ivarref/bp-diagrams/master/data/slim-2.json"))))
 (def cc3 (json/read-str (:body (cached-get "https://raw.githubusercontent.com/ivarref/bp-diagrams/master/data/slim-3.json"))))
+
+(def urls {"https://raw.githubusercontent.com/ivarref/bp-diagrams/master/data/population.csv" :population
+           "https://raw.githubusercontent.com/ivarref/bp-diagrams/master/data/gdp.csv" :gdp})
+
+(defn parse-url [url prop]
+  (let [resp (cached-get url)
+        expected-columns [(keyword "Country Code") (keyword "Country Name") :Year :Value]
+        csv-map (csv/csv-map (:body resp))]
+    (test/is (= 200 (:status resp)))
+    (->> csv-map
+         (csv/assert-columns expected-columns)
+         (:data)
+         (csv/read-string-columns (:columns csv-map))
+         (csv/number-or-nil-columns [:Year :Value])
+         (mapv #(set/rename-keys % {:Year :year
+                                    (keyword "Country Name") :country
+                                    :Value prop})))))
+
+(def all-data (->> urls
+                   (mapv #(parse-url (first %) (second %)))
+                   (flatten)
+                   (group-by (fn [x] [(:year x) (:country x)]))
+                   (vals)
+                   (mapv #(reduce merge {} %))))
 
 (def cc3-to-cc2
   (->> [cc2 cc3]
