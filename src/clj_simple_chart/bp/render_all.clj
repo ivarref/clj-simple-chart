85df53410cb0c1ff95a8fd49709ca67e10fa620c
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-07-01 14:23:02 +0200

    More refactoring

---
+++ src/clj_simple_chart/bp/render_all.clj
@@ -0,0 +1,19 @@
+(ns clj-simple-chart.bp.render-all
+  (:require [clj-simple-chart.bp.diagrams.co-per-capita-sel]
+            [clj-simple-chart.bp.diagrams.gas-consumption-per-capita]
+            [clj-simple-chart.bp.diagrams.gas-production-per-capita]
+            [clj-simple-chart.bp.diagrams.oil-consumption-per-capita]
+            [clj-simple-chart.bp.diagrams.oil-net-imports]
+            [clj-simple-chart.bp.diagrams.oiloil-production-per-capita]
+            [clj-simple-chart.bp.diagrams.renewables-share]
+            [clj-simple-chart.bp.diagrams.renewables-share-sel]))
+
+(defn render-all []
+  (clj-simple-chart.bp.diagrams.co-per-capita-sel/render-self)
+  (clj-simple-chart.bp.diagrams.gas-consumption-per-capita/render-self)
+  (clj-simple-chart.bp.diagrams.gas-production-per-capita/render-self)
+  (clj-simple-chart.bp.diagrams.oil-consumption-per-capita/render-self)
+  (clj-simple-chart.bp.diagrams.oil-net-imports/render-self)
+  (clj-simple-chart.bp.diagrams.oiloil-production-per-capita/render-self)
+  (clj-simple-chart.bp.diagrams.renewables-share/render-self)
+  (clj-simple-chart.bp.diagrams.renewables-share-sel/render-self))
