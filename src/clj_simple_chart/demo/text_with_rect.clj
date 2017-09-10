982dd8506d4983ab775d36860a663cac08707181
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-09-10 13:15:42 +0200

    ...

--- src/clj_simple_chart/demo/text_with_rect.clj
+++ src/clj_simple_chart/demo/text_with_rect.clj
@@ -12,25 +12,26 @@
 (defn diagram []
   [:svg {:xmlns "http://www.w3.org/2000/svg" :width svg-width :height svg-height}
    [:g {:transform (translate 10 10)}
-    [:path {:d "M0 0 V250"
-            :fill "none"
-            :stroke "red"
-            :stroke-width "1px"}]
-    [:path {:d "M23 0 V250"
-            :fill "none"
-            :stroke "red"
-            :stroke-width "1px"}]
+    ;[:path {:d "M0 0 V250"
+    ;        :fill "none"
+    ;        :stroke "red"
+    ;        :stroke-width "1px"}]
+    ;[:path {:d "M23 0 V250"
+    ;        :fill "none"
+    ;        :stroke "red"
+    ;        :stroke-width "1px"}]
 
     (opentype/stack {}
-                    [{:text "My great heading" :font "Roboto Black" :font-size font-size}
+                    [
+                     {:text "My great heading" :font "Roboto Black" :font-size font-size}
                      {:text "It's great" :font "Roboto Black" :font-size 16}
+                     {:text "\"It's really great\", -Maja" :font "Roboto Black" :font-size 16}
 
-                     {:text "Abc" :font "Roboto Black" :font-size font-size :rect {:fill "red" :size rect-size}}
-                     {:text "Def" :font "Roboto Black" :font-size font-size :rect {:fill "green" :size rect-size}}
-                     {:text "Ghi" :font "Roboto Black" :font-size font-size :rect {:fill "orange" :size rect-size}}
-                     {:text "ABC" :font "Roboto Black" :font-size font-size :rect {:fill "cyan" :size rect-size}}
-                     {:text "OMG" :font "Roboto Black" :font-size font-size :rect {:fill "yellow" :size rect-size}}
-
+                     {:text "Abc" :font "Roboto Bold" :font-size font-size :rect {:fill "red" :size rect-size}}
+                     {:text "Def" :font "Roboto Bold" :font-size font-size :rect {:fill "green" :size rect-size}}
+                     {:text "Ghi" :font "Roboto Bold" :font-size font-size :rect {:fill "orange" :size rect-size}}
+                     {:text "Spq" :font "Roboto Bold" :font-size font-size :rect {:fill "cyan" :size rect-size}}
+                     {:text "OMLLLLL" :font "Roboto Bold" :font-size font-size :rect {:fill "yellow" :size rect-size}}
                      ])
 
     ]])
