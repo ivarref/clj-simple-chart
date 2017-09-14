1f85713ffaaea862e1f2bf42f3c895b66d2f7df7
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-09-14 19:39:12 +0200

    Refactor reserve code

--- src/clj_simple_chart/ncs/reserve.clj
+++ src/clj_simple_chart/ncs/reserve.clj
@@ -1,11 +1,14 @@
 (ns clj-simple-chart.ncs.reserve
   (:require [clj-http.client :as client]
-            [clj-simple-chart.csv.csvmap :as csvmap]
+            [clj-simple-chart.csv.csvmap :as csv]
             [clojure.pprint :as pprint]
             [clojure.test :as test]))
 
 (def field-reserves-url "http://factpages.npd.no/ReportServer?/FactPages/TableView/field_reserves&rs:Command=Render&rc:Toolbar=false&rc:Parameters=f&rs:Format=CSV&Top100=false&IpAddress=80.213.237.130&CultureCode=en")
-(defonce field-reserves-raw (-> field-reserves-url client/get :body csvmap/csv-map))
+(defonce field-reserves-raw (-> field-reserves-url
+                                (client/get)
+                                (:body)
+                                (csv/csv-map)))
 
 (def columns (:columns field-reserves-raw))
 (def data (:data field-reserves-raw))
@@ -25,34 +28,26 @@
                      :fldNpdidField
                      :DatesyncNPD]))
 
-(def numeric-fields [:fldRecoverableOil
-                     :fldRecoverableGas
-                     :fldRecoverableNGL
-                     :fldRecoverableCondensate
-                     :fldRecoverableOE
-                     :fldRemainingOil
-                     :fldRemainingGas
-                     :fldRemainingNGL
-                     :fldRemainingCondensate
-                     :fldRemainingOE])
+(def numeric-columns [:fldRecoverableOil
+                      :fldRecoverableGas
+                      :fldRecoverableNGL
+                      :fldRecoverableCondensate
+                      :fldRecoverableOE
+                      :fldRemainingOil
+                      :fldRemainingGas
+                      :fldRemainingNGL
+                      :fldRemainingCondensate
+                      :fldRemainingOE])
 
-(defn parse-row [row]
-  (reduce (fn [r field] (update r field
-                                (fn [x]
-                                  (if (number? (read-string x))
-                                    (read-string x)
-                                    (throw (Exception. (str "Expected number, got " x)))))))
-          row
-          numeric-fields))
-
-(def data-parsed (map parse-row data))
+(def data-parsed (->> data
+                      (csv/read-string-columns numeric-columns)
+                      (csv/number-or-throw-columns numeric-columns)))
 
 (def field-names (map :fldName data))
 
 (defn get-reserve [field-name kind]
   {:pre [(some #{field-name} field-names)
          (some #{kind} [:fldRecoverableOE :fldRecoverableOil :fldRecoverableGas])]}
-  (-> (filter #(= (:fldName %) field-name) data)
+  (-> (filter #(= (:fldName %) field-name) data-parsed)
       first
-      (get kind)
-      read-string))
+      (get kind)))
