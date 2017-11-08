d3594169bb2d59c47ce8dc07156e39be810cf914
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-11-08 18:48:10 +0100

    Add avia-paoc end of year

--- src/clj_simple_chart/eurostat/avia_paoc.clj
+++ src/clj_simple_chart/eurostat/avia_paoc.clj
@@ -1,7 +1,8 @@
 (ns clj-simple-chart.eurostat.avia-paoc
   (:require [clj-http.client :as client]
             [clojure.test :as test]
-            [clj-simple-chart.csv.csvmap :as csvmap])
+            [clj-simple-chart.csv.csvmap :as csvmap]
+            [clojure.string :as str])
   (:import (org.apache.commons.compress.archivers ArchiveStreamFactory)
            (java.io ByteArrayInputStream ByteArrayOutputStream)
            (org.apache.commons.compress.compressors.gzip GzipCompressorInputStream)
@@ -19,9 +20,45 @@
 (defonce output (ByteArrayOutputStream.))
 (defonce input (GzipCompressorInputStream. (ByteArrayInputStream. (:body response))))
 
-(defonce side-effect (IOUtils/copy input output)) ; such code.
+(defonce side-effect (IOUtils/copy input output))           ; such code.
 (defonce tsv-bytearray (.toByteArray output))
 (spit "data/eurostat/avia-paoc.tsv" (String. tsv-bytearray StandardCharsets/UTF_8))
 (defonce tsv (csvmap/tsv-map (String. tsv-bytearray StandardCharsets/UTF_8)))
 
-;(def unpacked (.createArchiveInputStream (ArchiveStreamFactory.) (ByteArrayInputStream. (:body response))))
+(def first-column-str "unit,tra_meas,geo,tra_cov,schedule\\time")
+(def regular-columns [:unit :geo :schedule-time :tra_cov :tra_meas])
+(def first-column (keyword first-column-str))
+(test/is (= first-column (first (:columns tsv))))
+
+(defn process-row [row]
+  (reduce (fn [o [idx v]]
+            (assoc o (keyword v)
+                     (nth (str/split (get row first-column) #",") idx)))
+          (dissoc row first-column)
+          (map-indexed (fn [idx x] [idx x]) (str/split first-column-str #","))))
+
+(defn remove-whitespace [row]
+  (reduce (fn [o [k v]]
+            (assoc o (keyword (str/trim (name k))) (str/trim v)))
+          {}
+          row))
+
+(defn condense-row [row]
+    (reduce (fn [o [k v]]
+              (if (or (some #{k} regular-columns)
+                      (= 4 (count (name k))))
+                (assoc o k v)
+                o))
+            {}
+            row))
+
+(def data (->> (:data tsv)
+               (mapv process-row)
+               (mapv remove-whitespace)
+               (mapv #(assoc % :schedule-time (get % (keyword "schedule\\time"))))
+               (mapv #(dissoc % (keyword "schedule\\time")))
+               (mapv condense-row)))
+
+(csvmap/write-csv "data/eurostat/avia-paoc-yearly.csv"
+                  {:columns (reverse (sort (keys (first data))))
+                   :data data})
