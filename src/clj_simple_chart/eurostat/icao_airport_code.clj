2125dbf660dab2ce1ad05d9115828520a5ae2213
Ivar Refsdal <refsdal.ivar@gmail.com>
2017-11-15 16:01:45 +0100

    ...

--- src/clj_simple_chart/eurostat/icao_airport_code.clj
+++ src/clj_simple_chart/eurostat/icao_airport_code.clj
@@ -2,4 +2,10 @@
 
 (def codes {"NO_ENGM" "Gardermoen"
             "NO_ENBR" "Flesland"
+            "NO_ENVA" "Værnes"
+            "NO_ENZV" "Sola"
+            "DK_EKCH" "Copenhagen"
+            "SE_ESSA" "Stockholm–Arlanda"
+            "NO_ENTC" "Tromsø lufthavn, Langnes"
+            "NO_ENBO" "Bodø lufthavn"
             })
