e413b8c1d93823dba3b5a64a60202d900e532492
Ivar Refsdal <refsdal.ivar@gmail.com>
2018-01-08 19:25:14 +0100

    First cut remaining oil reserves over time

---
+++ src/clj_simple_chart/ncs/reserves/oil_reserves_over_time.clj
@@ -0,0 +1,147 @@
+(ns clj-simple-chart.ncs.reserves.oil-reserves-over-time
+  (:require [clj-simple-chart.ncs.johan-highlight-data :as datasource]
+            [clj-simple-chart.ncs.production-cumulative-yearly-fields :as production]
+            [clj-simple-chart.core :as core]
+            [clj-simple-chart.colors :refer :all]
+            [clj-simple-chart.opentype :as opentype]
+            [clj-simple-chart.axis.core :as axis]
+            [clj-simple-chart.chart :as chart]
+            [clj-simple-chart.translate :refer [translate translate-y]]
+            [clj-simple-chart.rect :as rect]
+            [clj-simple-chart.rect :refer [bars]]
+            [clj-simple-chart.point :as point]
+            [clojure.string :as string]
+            [clojure.string :as str]))
+
+(def data datasource/exploded-data-liquids-gboe)
+
+(def max-year (apply max (map :year data)))
+
+(def colors {"EKOFISK"        blue
+             "STATFJORD"      red
+             "VALHALL"        orange
+             "GULLFAKS"       "#ffaa00"
+             "SNORRE"         "#9467bd"
+             "TROLL"          green
+             "OSEBERG"        cyan
+             "ÅSGARD"         hydro-blue
+             "HEIDRUN"        "#bcbd22"
+             "OTHERS"         "#969696" ;""#7f7f7f"
+             "JOHAN SVERDRUP" dark-purple
+             "JOHAN CASTBERG" pink})
+
+;{:not-evaluated          hydro-blue
+; :likely                 brown
+; :clarification          "#ffaa00"
+; :decided-for-production orange
+; :pdo-approved           "#109618"              ; hard green
+; :remaining-reserves     red
+; :producing-produced     "#eA6F6F"
+; :shut-down-produced     gray}
+
+(def marg 10)
+(def two-marg (* 2 marg))
+
+(def svg-width 900)
+(def svg-height 500)
+
+(def available-width (- svg-width (* 2 marg)))
+
+(def x-domain (->> data
+                   (map :year)
+                   (distinct)
+                   (sort)
+                   (vec)))
+
+(def columns (mapv first datasource/cat-name-and-flds))
+
+(def xx {:type          :ordinal
+         :orientation   :bottom
+         :tick-values   (vec (distinct (flatten [(first x-domain)
+                                                 (range 1970 2017 5)])))
+         ;(last x-domain)])))
+         :tick-format   (fn [x] (cond (= x 1967) "67"
+                                      (= x 2017) "17"
+                                      :else x))
+         :domain        x-domain
+         :sub-domain    columns
+         :padding-inner 0.1
+         :padding-outer 0.1})
+
+(def yy {:type               :linear
+         :orientation        :right
+         :grid               true
+         :axis-text-style-fn (fn [x] {:font "Roboto Bold"})
+         :domain             [0 26]})
+
+(def header (opentype/stack
+              {:width available-width}
+              [{:text "Gjenverande oljereservar* over tid" :font "Roboto Bold" :font-size 30}
+               {:text "Dei ti største oljefelta, Johan Castberg og alle andre felt" :font "Roboto Bold" :font-size 16}
+               {:text          "Milliardar fat olje" :font "Roboto Bold" :font-size 16
+                :align         :right
+                :margin-bottom 3
+                :valign        :bottom}]))
+
+(def footer (opentype/stack
+              {:width available-width}
+              [{:margin-top 5 :text "*Inkluderer ressursklassane 1, 2 og 3." :font "Roboto Regular" :font-size 14}
+               {:margin-top 2 :text "Kjelde: ODs Faktasider. \"Olje\" inkluderer råolje, kondensat og NGL." :font "Roboto Regular" :font-size 14}
+               {:text "Diagram © Refsdal.Ivar@gmail.com" :font "Roboto Regular" :font-size 14 :valign :bottom :align :right}]))
+
+(def available-height (- svg-height (+ (+ 3 marg)
+                                       (:height (meta header))
+                                       (:height (meta footer)))))
+
+(def c (chart/chart {:width  available-width
+                     :height available-height
+                     :x      xx
+                     :y      yy}))
+
+(def txt {"OTHERS"         (str "Alle andre "
+                                (count datasource/reserve-field-names)
+                                " felt")
+          "JOHAN CASTBERG" "Johan Castberg"
+          "JOHAN SVERDRUP" "Johan Sverdrup"
+          "HEIDRUN"        "Heidrun"
+          "ÅSGARD"         "Åsgard"
+          "OSEBERG"        "Oseberg"
+          "TROLL"          "Troll"
+          "SNORRE"         "Snorre"
+          "GULLFAKS"       "Gullfaks"
+          "VALHALL"        "Valhall"
+          "STATFJORD"      "Statfjord"
+          "EKOFISK"        "Ekofisk"})
+
+(def cat-to-value-last-year (->> data
+                                 (filter #(= (:year %) max-year))
+                                 (map (juxt :c :value))
+                                 (into {})))
+
+(defn diagram []
+  [:svg {:xmlns "http://www.w3.org/2000/svg" :width svg-width :height svg-height}
+   [:g {:transform (translate marg marg)}
+    header
+    [:g {:transform (translate (:margin-left c) (+ (:height (meta header)) (:margin-top c)))}
+     (axis/render-axis (:x c))
+     (axis/render-axis (:y c))
+     (bars c {:p :year :h :value :fill colors} data)
+     [:g {:transform (translate 10 15)}
+      (opentype/stack {:widht        available-width
+                       :fill         "whitesmoke"
+                       :fill-opacity 0.8
+                       :margin       5}
+                      (vec (flatten [{:text "Felt" :font "Roboto Black" :font-size 16}
+                                     ;:right {:text "Mrd. fat olje"}}
+                                     (map (fn [col]
+                                            {:text      (get txt col col)
+                                             :font      "Roboto Regular"
+                                             :font-size 16
+                                             ;:right     {:text (str/replace (format "%.1f" (get cat-to-value-last-year col)) "." ",")}
+                                             :rect      {:fill (get colors col)}})
+                                          (reverse columns))])))]]
+    ;{:text  "Totalt" :font "Roboto Bold" :font-size 16 :right {:text (str/replace (format "%.1f" (reduce + 0 (vals cat-to-value-last-year))) "." ",")}}])))]]
+    [:g {:transform (translate-y (+ (:height (meta header)) available-height))} footer]]])
+
+(defn render-self []
+  (core/render "./img/ncs-svg/remaining-reserves-oil.svg" "./img/ncs-png/remaining-reserves-oil.png" (diagram)))
