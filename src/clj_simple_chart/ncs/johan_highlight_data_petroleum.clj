9a91e5374ed6180af79b71188afe1b3f99989556
Ivar Refsdal <refsdal.ivar@gmail.com>
2018-01-10 16:23:28 +0100

    Add petro data

---
+++ src/clj_simple_chart/ncs/johan_highlight_data_petroleum.clj
@@ -0,0 +1,105 @@
+(ns clj-simple-chart.ncs.johan-highlight-data-petroleum
+  (:require [clj-simple-chart.ncs.discovery :as discovery]
+            [clj-simple-chart.ncs.production-cumulative-yearly-fields :as production]
+            [clojure.test :as test]
+            [clj-simple-chart.csv.csvmap :as csv]))
+
+; decided for production
+; pdo approved
+; producing and shut-down
+(def reserve-type [:pdo-approved :decided-for-production :producing :shut-down])
+
+(def reserves (->> discovery/parsed
+                   (filter #(some #{(:status %)} reserve-type))))
+
+(def reserve-field-names (->> reserves
+                              (map :name)
+                              (distinct)
+                              (sort)
+                              (vec)))
+
+(def top-11-players-names (conj (->> reserves
+                                     (sort-by :petroleum)
+                                     (take-last 10)
+                                     (sort-by :year)
+                                     (mapv :name))
+                                "JOHAN CASTBERG"))
+
+(test/is (= top-11-players-names
+            ["EKOFISK" "STATFJORD" "GULLFAKS" "SNORRE" "OSEBERG" "TROLL" "ÅSGARD" "SNØHVIT" "ORMEN LANGE"
+             "JOHAN SVERDRUP" "JOHAN CASTBERG"]))
+
+(def other-fields (->> reserves
+                       (remove #(some #{(:name %)} top-11-players-names))
+                       (sort-by :name)))
+
+(def pre-1990 (->> other-fields
+                   (filter #(< (:year %) 1990))
+                   (sort-by :name)
+                   (mapv :name)))
+
+(def post-1990 (->> other-fields
+                    (filter #(>= (:year %) 1990))
+                    (filter #(< (:year %) 2000))
+                    (sort-by :name)
+                    (mapv :name)))
+
+(def post-2000 (->> other-fields
+                    (filter #(>= (:year %) 2000))
+                    (sort-by :name)
+                    (mapv :name)))
+
+;(test/is (some #{"GOLIAT"} post-1990))
+(test/is (some #{"VEST EKOFISK"} pre-1990))
+
+(def top-fields (->> reserves
+                     (filter #(some #{(:name %)} top-11-players-names))))
+
+(def cats (vec (flatten [(drop-last 2 top-11-players-names)
+                         "OTHERS_PRE_1990"
+                         "OTHERS_POSTE_1990"
+                         "OTHERS_POSTE_2000"
+                         "JOHAN SVERDRUP"
+                         "JOHAN CASTBERG"])))
+
+(def override-default {"OTHERS_PRE_1990"   pre-1990
+                       "OTHERS_POSTE_1990" post-1990
+                       "OTHERS_POSTE_2000" post-2000})
+
+(def cat-name-and-flds (mapv (fn [f] [f (get override-default f [f])])
+                             cats))
+
+(defn remaining-at-time [flds year kind]
+  (let [original-recoverable (->> reserves
+                                  (filter #(some #{(:name %)} flds))
+                                  (filter #(>= year (:year %)))
+                                  (map kind)
+                                  (reduce + 0))
+        produced (production/cumulative-production (filter #(some #{%} production/field-names) flds)
+                                                   year
+                                                   kind)]
+    (- original-recoverable produced)))
+
+(def flat-data-petroleum
+  (for [yr (range discovery/start-year (inc discovery/stop-year))]
+    (reduce (fn [o [v flds]] (assoc o v (remaining-at-time flds yr :petroleum)))
+            {"YEAR" yr} cat-name-and-flds)))
+
+(defn explode-row [row]
+  (reduce (fn [o k]
+            (conj o {:year  (get row "YEAR")
+                     :c     k
+                     :value (get row k)}))
+          [] (mapv first cat-name-and-flds)))
+
+(def exploded-data-petroleum-gboe
+  (->> flat-data-petroleum
+       (mapcat explode-row)
+       (map #(update % :value (fn [v] (double (* 6.29e-3 v)))))
+       (sort-by :year)
+       (vec)))
+
+(csv/write-csv-format "data/ncs/discovery/johan-highlight-petroleum.csv"
+                      {:columns (vec (cons "YEAR" (mapv first cat-name-and-flds)))
+                       :data    flat-data-petroleum
+                       :format  (reduce (fn [o [k v]] (assoc o k "%.1f")) {} cat-name-and-flds)})
---
+++ src/clj_simple_chart/ncs/reserves/petroleum_reserves_over_time.clj
@@ -0,0 +1,144 @@
+(ns clj-simple-chart.ncs.reserves.petroleum-reserves-over-time
+  (:require [clj-simple-chart.ncs.johan-highlight-data-petroleum :as datasource]
+            [clj-simple-chart.ncs.production-cumulative-yearly-fields :as production]
+            [clj-simple-chart.core :as core]
+            [clj-simple-chart.colors :refer :all]
+            [clj-simple-chart.opentype :as opentype]
+            [clj-simple-chart.axis.core :as axis]
+            [clj-simple-chart.chart :as chart]
+            [clj-simple-chart.translate :refer [translate translate-y]]
+            [clj-simple-chart.rect :as rect]
+            [clj-simple-chart.rect :refer [bars]]
+            [clj-simple-chart.point :as point]
+            [clojure.string :as string]
+            [clojure.string :as str]))
+
+(def data datasource/exploded-data-petroleum-gboe)
+
+(def max-year (apply max (map :year data)))
+
+(def colors {"EKOFISK"           blue
+             "STATFJORD"         red
+             "GULLFAKS"          orange
+             "SNORRE"            "#ffaa00"
+             "OSEBERG"           "#9467bd"
+             "TROLL"             green
+             "ÅSGARD"            cyan
+             "SNØHVIT"           hydro-blue
+             "ORMEN LANGE"       "#bcbd22"
+             "OTHERS_PRE_1990"   "#969696"                  ;""#7f7f7f"
+             "OTHERS_POSTE_1990" "#a1d99b"                  ;"#B6b6b6"
+             "OTHERS_POSTE_2000" "rgb(231, 186, 82)"
+             "JOHAN SVERDRUP"    dark-purple
+             "JOHAN CASTBERG"    pink})
+
+(def marg 10)
+(def two-marg (* 2 marg))
+
+(def svg-width 900)
+(def svg-height 500)
+
+(def available-width (- svg-width (* 2 marg)))
+
+(def x-domain (->> data
+                   (map :year)
+                   (distinct)
+                   (sort)
+                   (vec)))
+
+(def columns (mapv first datasource/cat-name-and-flds))
+
+(def xx {:type          :ordinal
+         :orientation   :bottom
+         :tick-values   (vec (distinct (flatten [(first x-domain)
+                                                 (range 1970 2017 5)])))
+         ;(last x-domain)])))
+         :tick-format   (fn [x] (cond (= x 1967) "67"
+                                      (= x 2017) "17"
+                                      :else x))
+         :domain        x-domain
+         :sub-domain    columns
+         :padding-inner 0.1
+         :padding-outer 0.1})
+
+(def yy {:type               :linear
+         :orientation        :right
+         :grid               true
+         :axis-text-style-fn (fn [x] {:font "Roboto Bold"})
+         :domain             [0 50]})
+
+(def header (opentype/stack
+              {:width available-width}
+              [{:text "Gjenverande petroleumsreservar* over tid" :font "Roboto Bold" :font-size 30}
+               {:text "Dei ti største petroleumsfelta, Johan Castberg og alle andre felt" :font "Roboto Bold" :font-size 16}
+               {:text          "Milliardar fat oljeekvivalentar" :font "Roboto Bold" :font-size 16
+                :align         :right
+                :margin-bottom 3
+                :valign        :bottom}]))
+
+(def footer (opentype/stack
+              {:width available-width}
+              [{:margin-top 5 :text "Kjelde: OD. *Inkluderer ressursklassane 1, 2 og 3. \"Olje\" inkluderer råolje, kondensat og NGL." :font "Roboto Regular" :font-size 14}
+               {:text "Diagram © Refsdal.Ivar@gmail.com" :font "Roboto Regular" :font-size 14 :valign :bottom :align :right}]))
+
+(def available-height (- svg-height (+ (+ 3 marg)
+                                       (:height (meta header))
+                                       (:height (meta footer)))))
+
+(def c (chart/chart {:width  available-width
+                     :height available-height
+                     :x      xx
+                     :y      yy}))
+
+(def txt {"OTHERS"            (str "Alle andre "
+                                   (count datasource/reserve-field-names)
+                                   " felt")
+          "OTHERS_PRE_1990"   (str "Andre felt funne før 1990")
+          "OTHERS_POSTE_1990" (str "Andre felt funne 1990–2000")
+          "OTHERS_POSTE_2000" (str "Andre felt funne f.o.m. 2000")
+          "JOHAN CASTBERG"    "Johan Castberg"
+          "JOHAN SVERDRUP"    "Johan Sverdrup"
+          "HEIDRUN"           "Heidrun"
+          "ÅSGARD"            "Åsgard"
+          "SNØHVIT"           "Snøkvit"
+          "OSEBERG"           "Oseberg"
+          "ORMEN LANGE"       "Ormen Lange"
+          "TROLL"             "Troll"
+          "SNORRE"            "Snorre"
+          "GULLFAKS"          "Gullfaks"
+          "VALHALL"           "Valhall"
+          "STATFJORD"         "Statfjord"
+          "EKOFISK"           "Ekofisk"})
+
+(def cat-to-value-last-year (->> data
+                                 (filter #(= (:year %) max-year))
+                                 (map (juxt :c :value))
+                                 (into {})))
+
+(defn diagram []
+  [:svg {:xmlns "http://www.w3.org/2000/svg" :width svg-width :height svg-height}
+   [:g {:transform (translate marg marg)}
+    header
+    [:g {:transform (translate (:margin-left c) (+ (:height (meta header)) (:margin-top c)))}
+     (axis/render-axis (:x c))
+     (axis/render-axis (:y c))
+     (bars c {:p :year :h :value :fill colors} data)
+     [:g {:transform (translate 10 15)}
+      (opentype/stack {:widht        available-width
+                       :fill         "whitesmoke"
+                       :fill-opacity 0.8
+                       :margin       5}
+                      (vec (flatten [{:text "Felt" :font "Roboto Black" :font-size 16}
+                                     ;:right {:text "Mrd. fat olje"}}
+                                     (map (fn [col]
+                                            {:text      (get txt col col)
+                                             :font      "Roboto Regular"
+                                             :font-size 16
+                                             ;:right     {:text (str/replace (format "%.1f" (get cat-to-value-last-year col)) "." ",")}
+                                             :rect      {:fill (get colors col)}})
+                                          (reverse columns))])))]]
+    ;{:text  "Totalt" :font "Roboto Bold" :font-size 16 :right {:text (str/replace (format "%.1f" (reduce + 0 (vals cat-to-value-last-year))) "." ",")}}])))]]
+    [:g {:transform (translate-y (+ (:height (meta header)) available-height))} footer]]])
+
+(defn render-self []
+  (core/render "./img/ncs-svg/remaining-reserves-petroleum.svg" "./img/ncs-png/remaining-reserves-petroleum.png" (diagram)))
