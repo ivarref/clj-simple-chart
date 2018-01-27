6648ed3068a45f7e031591015aa6822315be14d1
Ivar Refsdal <refsdal.ivar@gmail.com>
2018-01-27 12:59:46 +0100

    Work on 11174

--- src/clj_simple_chart/ssb/data/sal_av_petroleumsprodukt_11174.clj
+++ src/clj_simple_chart/ssb/data/sal_av_petroleumsprodukt_11174.clj
@@ -1,8 +1,24 @@
 (ns clj-simple-chart.ssb.data.sal-av-petroleumsprodukt-11174
-  (:require [clj-simple-chart.ssb.data.ssb-pull :as sf]))
+  (:require [clj-simple-chart.ssb.data.ssb-api :as ssb]
+            [clojure.test :as test]
+            [clj-simple-chart.csv.csvmap :as csv]))
 
-(def data (sf/pull-parse 11174 {"ContentsCode" "Salg"
+; 11174: Salg av petroleumsprodukter, etter kjøpegruppe og produkttype (mill. liter). Foreløpige tall (F) 2010M01 - 2017M12
+;
+; https://www.ssb.no/statbank/table/11174?rxid=49a52ff4-5d3c-4264-aa49-95134312070d
+
+(def raw-data (ssb/fetch 11174 {"ContentsCode" "Salg"
                                 "Region" "Hele landet"
                                 "PetroleumProd" ["Autodiesel" "Bilbensin"]
                                 "Kjopegrupper" "Alle kjøpegrupper"
                                 "Tid" "*"}))
+
+(test/is (= [:kjøpegruppe :petroleumsprodukt :region :salg] (:columns raw-data)))
+
+(def data (->> (:data raw-data)
+               (sort-by :dato)
+               (csv/number-or-throw-columns [:salg])
+               (map #(dissoc % :kjøpegruppe))
+               (map #(dissoc % :region))))
+
+(test/is (= "2010-01" (:dato (first data))))
