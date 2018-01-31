3d7218cb95800b9932d933582b36ca780ea47e8f
Ivar Refsdal <refsdal.ivar@gmail.com>
2018-01-31 21:34:41 +0100

    Add basic gjeld ssb/fetch

---
+++ src/clj_simple_chart/ssb/data/gjeld.clj
@@ -0,0 +1,14 @@
+(ns clj-simple-chart.ssb.data.gjeld
+  (:require [clj-simple-chart.ssb.data.ssb-api :as ssb]
+            [clj-simple-chart.data.utils :refer :all]
+            [clojure.set :as set]))
+
+(def q {"Valuta" "I alt"
+        "Lantaker2" "Husholdninger mv."
+        "ContentsCode" "Transaksjoner, tolvmånedersvekst (prosent)"
+        "Tid" "*"})
+
+(def data (->> (ssb/fetch 11599 q)
+               (:data)
+               (map #(set/rename-keys % {(keyword "transaksjoner, tolvmånedersvekst (prosent)") :content}))
+               (remove-nils)))
