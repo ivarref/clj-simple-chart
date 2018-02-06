e2e054d8fa50e4f668065834d2710254ba8e3ad1
Ivar Refsdal <refsdal.ivar@gmail.com>
2018-02-06 18:37:34 +0100

    Start make lonn vs gjeld

--- src/clj_simple_chart/ssb/data/gjeld.clj
+++ src/clj_simple_chart/ssb/data/gjeld.clj
@@ -1,6 +1,7 @@
 (ns clj-simple-chart.ssb.data.gjeld
   (:require [clj-simple-chart.ssb.data.ssb-api :as ssb]
             [clj-simple-chart.data.utils :refer :all]
+            [clj-simple-chart.ssb.data.lønnsvekst :as lonnsvekst]
             [clojure.set :as set]
             [clojure.string :as str]))
 
@@ -11,4 +12,9 @@
 
 (def data (->> (ssb/fetch 11599 q)
                (remove-nils)
-               (filter #(str/ends-with? (:Tid %) "-12"))))
+               (keep-columns [:Tid :ContentsCode])
+               (filter #(str/ends-with? (:Tid %) "-12"))
+               (map #(set/rename-keys % {:ContentsCode :gjeldsvekst}))
+               (map #(update % :Tid (fn [x] (subs x 0 4))))
+               (map #(assoc % :lonnsvekst (get lonnsvekst/data (read-string (:Tid %)))))
+               (remove-nils)))
