9c65904baeaa4316b8165138a69ac3ef9b670487
Ivar Refsdal <refsdal.ivar@gmail.com>
2018-02-06 19:31:44 +0100

    ...

---
+++ src/clj_simple_chart/ssb/diagrams/gjeld_vs_inntekt.clj
@@ -0,0 +1,58 @@
+(ns clj-simple-chart.ssb.diagrams.gjeld-vs-inntekt
+  (:require [clj-simple-chart.ssb.data.gjeld :as datasource]
+    [clojure.string :as str]
+    [clj-simple-chart.translate :refer [translate translate-y]]
+    [clj-simple-chart.line :refer [line]]
+    [clj-simple-chart.rect :refer [bars]]
+    [clj-simple-chart.chart :as chart]
+    [clj-simple-chart.axis.core :as axis]
+    [clj-simple-chart.core :as core]
+    [clojure.string :as string]
+    [clj-simple-chart.opentype :as opentype]))
+
+(def data datasource/data)
+
+(def marg 10)
+(def two-marg (* 2 marg))
+(def svg-width 900)
+(def svg-height 500)
+(def available-width (- svg-width (* 2 marg)))
+
+(def xx {:type          :ordinal
+         :orientation   :bottom
+         :tick-values   (map :Tid data)
+         :domain        (map :Tid data)
+         :padding-inner 0.1
+         :padding-outer 0.1})
+
+(def yy {:type               :linear
+         :orientation        :right
+         :grid               true
+         :axis-text-style-fn (fn [x] {:font "Roboto Bold"})
+         :domain             [0 30]})
+
+(def header (opentype/stack
+              {:width available-width}
+              [{:text "Auke i gjeld vs inntekt" :font "Roboto Bold" :font-size 30}
+               {:text "Prosent" :font "Roboto Bold" :font-size 16 :valign :bottom :align :right}]))
+
+(def available-height (- svg-height (+ (+ 3 marg)
+                                       (:height (meta header)))))
+
+(def c (chart/chart {:width  available-width
+                     :height available-height
+                     :x      xx
+                     :y      yy}))
+
+(defn diagram []
+  [:svg {:xmlns "http://www.w3.org/2000/svg" :width svg-width :height svg-height}
+   [:g {:transform (translate marg marg)}
+    header
+    [:g {:transform (translate (:margin-left c) (+ (:height (meta header)) (:margin-top c)))}
+     (axis/render-axis (:x c))
+     (axis/render-axis (:y c))
+     (bars c {:p :Tid :h [[:gjeldsvekst "red"] [:lonnsvekst "green"]]} data)]]]) ; TODO: Support this style...
+
+
+(defn render-self []
+  (core/render "tmp.svg" "tmp.png" (diagram)))
