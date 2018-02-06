f6c1eb2e672b08f962728677d1c98142f7404ddb
Ivar Refsdal <refsdal.ivar@gmail.com>
2018-02-06 17:38:59 +0100

    Add lønnsvekst

---
+++ src/clj_simple_chart/ssb/data/lønnsvekst.clj
@@ -0,0 +1,21 @@
+(ns clj-simple-chart.ssb.data.lønnsvekst)
+
+(def data (zipmap (range 2000 2018)
+                  [4.9
+                   4.6
+                   6.7
+                   3.4
+                   3.6
+                   3.2
+                   4.7
+                   5.6
+                   6.1
+                   3.5
+                   4.0
+                   3.8
+                   3.9
+                   3.5
+                   3.2
+                   2.6
+                   1.6
+                   2.3]))
