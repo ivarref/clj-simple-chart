28c49172dbead035e6ef744aad93a2514e043300
Ivar Refsdal <refsdal.ivar@gmail.com>
2018-02-28 18:55:36 +0100

    Compare 2016 with 2017 for reserves

---
+++ img/ncs-png/reserves-history/year-vs-year.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -ex
+rm -fv 0.png
+rm -fv 1.png
+
+curl https://raw.githubusercontent.com/ivarref/clj-simple-chart/075728e7122efc52ef34b468294444c207f68e44/img/ncs-png/remaining-reserves-oil.png -o 0.png
+curl https://raw.githubusercontent.com/ivarref/clj-simple-chart/master/img/ncs-png/remaining-reserves-oil.png -o 1.png
+montage [0-1].png -tile 2x1 -geometry +0+0 -scale 50% oil.png
+
+curl https://raw.githubusercontent.com/ivarref/clj-simple-chart/075728e7122efc52ef34b468294444c207f68e44/img/ncs-png/remaining-reserves-gas.png -o 0.png
+curl https://raw.githubusercontent.com/ivarref/clj-simple-chart/master/img/ncs-png/remaining-reserves-gas.png -o 1.png
+montage [0-1].png -tile 2x1 -geometry +0+0 -scale 50% gas.png
+
+curl https://raw.githubusercontent.com/ivarref/clj-simple-chart/075728e7122efc52ef34b468294444c207f68e44/img/ncs-png/remaining-reserves-petroleum.png -o 0.png
+curl https://raw.githubusercontent.com/ivarref/clj-simple-chart/master/img/ncs-png/remaining-reserves-petroleum.png -o 1.png
+montage [0-1].png -tile 2x1 -geometry +0+0 -scale 50% petroleum.png
