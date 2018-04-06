bdeda6d40849c9ff22526eddfd3841d2a7424d7f
Ivar Refsdal <ivar.refsdal@nsd.no>
2018-04-06 09:09:31 +0200

    Update netto konstantstraum

--- src/clj_simple_chart/ssb/nettokontantstraum/nettokontantstraumdiagram.clj
+++ src/clj_simple_chart/ssb/nettokontantstraum/nettokontantstraumdiagram.clj
@@ -182,7 +182,8 @@
                     2013
                     2014
                     2015
-                    2016])
+                    2016
+                    2017])
 
 (def end-of-year-data (->> data
                            (filter #(and (some #{(:year %)} txt-for-years)
