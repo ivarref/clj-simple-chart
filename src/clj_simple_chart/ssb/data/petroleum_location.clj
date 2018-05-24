a301e810015631442710a4815fbce17ff2dedef2
Ivar Refsdal <refsdal.ivar@gmail.com>
2018-05-24 20:20:30 +0200

    ...

---
+++ src/clj_simple_chart/ssb/data/petroleum_location.clj
@@ -0,0 +1,33 @@
+(ns clj-simple-chart.ssb.data.petroleum-location
+  (:require [clj-simple-chart.ssb.data.ssb-api :as ssb]
+            [clojure.test :as test]
+            [clj-simple-chart.data.utils :refer :all]
+            [clj-simple-chart.csv.csvmap :as csv]
+            [clojure.string :as str]
+            [clojure.set :as set]))
+
+; 11174: Salg av petroleumsprodukter, etter kjøpegruppe og produkttype (mill. liter). Foreløpige tall (F) 2010M01 - 2017M12
+;
+; https://www.ssb.no/statbank/table/11174?rxid=49a52ff4-5d3c-4264-aa49-95134312070d
+
+(defn data
+  [location]
+  (->> (ssb/fetch 11174 {[:ContentsCode :as :salg]               "Salg"
+                         "Region"                                location
+                         [:PetroleumProd :as :petroleumsprodukt] ["Autodiesel" "Bilbensin"]
+                         [:Kjopegrupper :as :kjøpegruppe]        "Alle kjøpegrupper"
+                         [:Tid :as :dato]                        "*"})
+       (map #(dissoc % :kjøpegruppe))
+       (map #(dissoc % :ContentsCodeCategory))
+       (map #(dissoc % :Region))
+       (csv/number-or-throw-columns [:salg])
+       (column-value->column :petroleumsprodukt)
+       (contract-by-column :dato)
+       (flat->12-mms)
+       (add-sum-column)))
+;(relative-to-all-time-high)))
+
+;(def data-relative (relative-to-all-time-high data))
+
+;(test/is (= "2010-12" (:dato (first data))))
+;(test/is (= "2018-04" (:dato (last data))))
---
+++ src/clj_simple_chart/ssb/diagrams/autos_noreg.clj
@@ -0,0 +1,77 @@
+(ns clj-simple-chart.ssb.diagrams.autos-noreg
+  (:require [clj-simple-chart.ssb.data.petroleum-location :as datasource]
+            [clojure.string :as str]
+            [clj-simple-chart.translate :refer [translate translate-y]]
+            [clj-simple-chart.rect :refer [bars]]
+            [clj-simple-chart.chart :as chart]
+            [clj-simple-chart.axis.core :as axis]
+            [clj-simple-chart.core :as core]
+            [clojure.string :as string]
+            [clj-simple-chart.opentype :as opentype]))
+
+(def data (datasource/data "Hele landet"))
+
+(def marg 10)
+(def two-marg (* 2 marg))
+(def svg-width 900)
+(def svg-height 500)
+(def available-width (- svg-width (* 2 marg)))
+
+(def xx {:type          :ordinal
+         :orientation   :bottom
+         :tick-values   (filter #(str/ends-with? % "-12") (map :dato data))
+         :tick-format   (fn [x] (str/replace x "-12" ""))
+         :domain        (map :dato data)
+         :padding-inner 0.1
+         :padding-outer 0.1})
+
+(def yy {:type               :linear
+         :orientation        :right
+         :grid               true
+         :axis-text-style-fn (fn [x] {:font "Roboto Bold"})
+         :domain             [0 (->> (map :sum data)
+                                     (apply max))]})
+
+(def y2 {:type               :linear
+         :orientation        :left
+         :axis-text-style-fn (fn [x] {:font "Roboto Bold"})
+         :domain             [0 100]})
+
+(def header (opentype/stack
+              {:width available-width}
+              [{:text "Sal av bensin og diesel, heile landet" :font "Roboto Bold" :font-size 30}
+               {:text "12 månadar glidande sum" :font "Roboto Bold" :font-size 16 :margin-bottom 10}
+               {:text "Autodiesel" :fill "red" :font "Roboto Bold" :font-size 16 :valign :bottom :align :right}
+               {:text "Bilbensin" :fill "green" :font "Roboto Bold" :font-size 16 :valign :bottom :align :right}
+               {:text "Millionar liter" :font "Roboto Bold" :font-size 16 :valign :bottom :align :right}]))
+
+
+(def footer (opentype/stack
+              {:width available-width}
+              [{:margin-top 6
+                :text       "Kjelde: SSB tabell 11174" :font "Roboto Regular" :font-size 14}
+               {:text "Diagram: refsdal.ivar@gmail.com" :font "Roboto Regular" :font-size 14 :valign :bottom :align :right}]))
+
+(def available-height (- svg-height (+ (+ 3 marg)
+                                       (:height (meta header))
+                                       (:height (meta footer)))))
+
+(def c (chart/chart {:width  available-width
+                     :height available-height
+                     :x      xx
+                     :y      yy}))
+;:y2     y2}))
+
+(defn diagram []
+  [:svg {:xmlns "http://www.w3.org/2000/svg" :width svg-width :height svg-height}
+   [:g {:transform (translate marg marg)}
+    header
+    [:g {:transform (translate (:margin-left c) (+ (:height (meta header)) (:margin-top c)))}
+     (bars c {:p :dato :h [[:bilbensin "green"] [:autodiesel "red"]]} data)
+     (axis/render-axis (:x c))
+     (axis/render-axis (:y c))]
+    ;(axis/render-axis (:y2 c))]
+    [:g {:transform (translate-y (+ (:height (meta header)) available-height))} footer]]])
+
+(defn render-self []
+  (core/render "img/ssb-svg/autos-noreg.svg" "img/ssb-png/autos-noreg.png" (diagram)))
