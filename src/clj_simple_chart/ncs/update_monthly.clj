b799fe73af626141fa9a2e74e69296cd745a0ec7
Ivar Refsdal <refsdal.ivar@gmail.com>
2018-07-14 21:51:23 +0200

    Update charts

--- src/clj_simple_chart/ncs/update_monthly.clj
+++ src/clj_simple_chart/ncs/update_monthly.clj
@@ -1,14 +1,29 @@
 (ns clj-simple-chart.ncs.update-monthly
   (:require [clj-simple-chart.ncs.liquidprodpp.liquid-pp-diagram-two]
+            [clj-simple-chart.ncs.liquidprodpp.liquid-pp-forecast]
             [clj-simple-chart.ncs.gas-rp-diagram]
-            [clj-simple-chart.ncs.petroprodpp.petroprodppdiagram]))
+            [clj-simple-chart.ncs.petroprodpp.petroprodppdiagram]
+            [clj-simple-chart.ssb.petroskatt.petroskattdiagrammonthly]
+            [clj-simple-chart.ssb.nettokontantstraum.nettokontantstraumdiagram]
+            [clj-simple-chart.ssb.diagrams.bensin-autodiesel]
+            [clj-simple-chart.ssb.diagrams.koyrelengde-etter-kategori]
+            [clj-simple-chart.ssb.diagrams.petroleumsprodukt-kjopegruppe]
+            [clj-simple-chart.ssb.diagrams.petroleumsprodukt-type]))
 
 
 (def namespaces '[clj-simple-chart.ncs.liquidprodpp.liquid-pp-diagram-two
                   clj-simple-chart.ncs.gas-rp-diagram
-                  clj-simple-chart.ncs.petroprodpp.petroprodppdiagram])
+                  clj-simple-chart.ncs.liquidprodpp.liquid-pp-forecast
+                  clj-simple-chart.ncs.petroprodpp.petroprodppdiagram
+                  clj-simple-chart.ssb.petroskatt.petroskattdiagrammonthly
+                  clj-simple-chart.ssb.nettokontantstraum.nettokontantstraumdiagram
+                  clj-simple-chart.ssb.diagrams.bensin-autodiesel
+                  clj-simple-chart.ssb.diagrams.koyrelengde-etter-kategori
+                  clj-simple-chart.ssb.diagrams.petroleumsprodukt-kjopegruppe
+                  clj-simple-chart.ssb.diagrams.petroleumsprodukt-type])
 
-(defn update-monthly []
+
+(defn render-self []
   (doseq [ns namespaces]
     (let [f (resolve (symbol (str ns) "render-self"))]
       (println "doing" ns "...")
